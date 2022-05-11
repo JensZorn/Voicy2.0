8557cb1b600aecfe9d7008a19c93caca5363b17b
skoester <sarah.koester0207@gmail.com>
2022-05-11 13:56:23 +0200

    clicked,dropdown

--- voicy/voicy_calendar.py
+++ voicy/voicy_calendar.py
@@ -21,6 +21,7 @@ from datetime import datetime
 from pytz import timezone
 from datetime import date
 from threading import Thread
+from tkinter import OptionMenu
 
 from time import strftime, sleep
 
@@ -54,7 +55,7 @@ class voicy_calendar():
         self.lbl.grid(row=1, column=0)
 
         self.cal = Calendar(self.frame, today=date.today())
-        self.cal.grid(sticky="nsew")
+        self.cal.grid(row= 0, column = 0, sticky="nsew")
 
         self.takeButton = ttk.Button(self.frame,
                                      text='Übernehme das Datum',
@@ -71,8 +72,38 @@ class voicy_calendar():
             parent.update_idletasks()
             sleep(1)
 
+    def clicked(self):
+        self.HoursList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16",
+                          "17", "18", "19", "20", "21", "22", "23", "24"]
+        self.MinuteList = ["00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"]
 
 
+
+
+
+        self.clicked=ttk.StringVar()
+        self.takeTime(textvariable="self.clicked")
+        self.clicked.set("self.HoursList[0]","self.MinuteList[00]")
+
+
+
+        self.button=ttk.Button(self.frame, text="Uhrzeit auswählen", width = 25, command = self.show)
+        self.button.grid()
+
+
+
+        self.dropdown = ttk.OptionMenu(self.frame, self.clicked, self.HoursList)
+        self.dropdown.config(width=80, font=("helvetica", 12))
+        self.dropdown.grid(row=3, column=2)
+
+        self.dropdown.minute = ttk.OptionMenu(self.frame, self.clicked, self.MinuteList)
+        self.dropdown.minute.config(width=80, font=("helvetica", 12))
+        self.dropdown.minute.grid(row=3, column=3)
+
+    #def callback(self):
+
+        #self.label.Uhrzeit.configure(text="Die ausgewählte Uhrzeit ist {}".format.self.takeTime.set())
+        #self.takeTime.trace(self.frame, self.callback)
 
 
 
